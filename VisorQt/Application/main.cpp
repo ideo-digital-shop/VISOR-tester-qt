4e99188dd002fe370781f51f971279998aaa4bc2
David <dbrody@ideo.com>
2014-07-18 07:10:21 -0700

    Changes Lemma name and makes it tied to VisorNoam

--- VisorQt/Application/main.cpp
+++ VisorQt/Application/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
         QQuickView view;
 
         //instantiate lemma with GuestName and desired RoomName (blank string for Free Guest).
-        Lemma * lemma = new Lemma(&view, "other", "");
+        Lemma * lemma = new Lemma(&view, "HMIModelController", "VisorNoam");
         lemma->start();
 
         qmlRegisterType<CursorShapeArea>("MyTools", 1, 0, "CursorShapeArea");
